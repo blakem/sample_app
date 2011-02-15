1f8f7d2c1cbed7d9d2ac33bc3f8146dcdccab1dc
Blake Mills <blakem30@yahoo.com>
2011-02-15 11:00:03 -0800

    Protecting edit and update with a before filter

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,6 +20,10 @@ module SessionsHelper
   def sign_out
     cookies.delete(:remember_token)
     current_user = nil
+  end  
+    
+  def deny_access
+    redirect_to signin_path, :notice => "Please sign in to access this page."
   end
   
   private
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 Factory.define :user do |user|
   user.name                  "Blake Mills"
-  user.email                 "blakem@blakem.com"
+  user.email                 "testuser@blakem.com"
   user.password              "foobar"
   user.password_confirmation "foobar"
 end
